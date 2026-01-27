ea42cf9e7a1dd9ce76729da0bbc874327b57df46
Akhil Gattu <akhilgattu02@gmail.com>
2026-01-27 23:41:59 +0530

    Refactor model code -Add generate_load_dataset.py and main.py to reorganize code into modular components -generate_load_dataset.py file for generating and loading dataset and main.py to train the model.

---
+++ generate_load_dataset.py
@@ -0,0 +1,150 @@
+'''
+Author: Akhil Gattu
+Date: 27th Jan 2026
+Script to generate and load IDRiD dataset in proper format to be 
+used by Dataset and DataLoader classes in torch.utils.data module 
+'''
+
+from torch.utils.data import Dataset, DataLoader
+import cv2, os
+import torch
+import os
+import numpy as np
+import cv2
+import albumentations as A
+
+image_paths = "/Users/akhilgattu/Desktop/VLM_project/Data/train/images/"
+mask_paths = "/Users/akhilgattu/Desktop/VLM_project/Data/train/masks/"
+
+'''
+Mapping abnormality to class id's
+'''
+
+
+'''
+Class to generate final dataset in a
+format that is readable by Dataset and DataLoader classes in torch.utils.data module 
+'''
+class IDRiDDatasetBuilder:
+    #Initialize dataset type and map each abnormality to an id
+    def __init__(self, dataset_type):
+        self.dataset_type = dataset_type
+        self.class_id_abnormality = {
+            "OD": 5,
+            "SE": 4,
+            "EX": 3,
+            "HE": 2,
+            "MA": 1 
+        }
+
+
+    #Resize image to (512, 512) resolution
+    def transform(self, img):
+        if len(img.shape) > 2:
+            img = img[..., 0]
+        img = cv2.resize(img, (512, 512))
+        return img
+
+    #Return masks of an image abnormality
+    def return_image_abnormality(self, dataset_type, abnormal_path, ab, h, w):
+        if dataset_type == "train":
+            msk_root = "A. Segmentation/2. All Segmentation Groundtruths/a. Training Set/"
+        
+        else:
+            msk_root = "A. Segmentation/2. All Segmentation Groundtruths/b. Testing Set/"
+
+        if ab in os.listdir(os.path.join(msk_root, abnormal_path)):
+            ab = os.path.join(msk_root, abnormal_path+ab)
+            img_ab = cv2.imread(ab, cv2.IMREAD_UNCHANGED)
+            img_ab = self.transform(img_ab)
+
+        else:
+            img_ab = np.zeros((h, w))
+        
+        return img_ab
+
+    '''
+    Create training and testing final dataset, where each tif file consists of all the masks combined 
+    and each pixel is a value in [0, 5] mapping class id to abnormailty.
+    '''
+    def create_dataset(self, dataset_type):
+
+        if dataset_type == "train":
+            root = "A. Segmentation/1. Original Images/a. Training Set/"
+        else:
+            root = "A. Segmentation/1. Original Images/b. Testing Set/"
+
+        img_root = "Data"
+        dir = os.listdir(root)
+        dir.sort()
+
+        if dataset_type not in os.listdir("/Users/akhilgattu/Desktop/VLM_project/"+img_root):
+            os.makedirs(img_root+"/"+dataset_type+"/images")
+            os.makedirs(img_root+"/"+dataset_type+"/masks")
+
+        for fol in dir:
+        
+            img_path = os.path.join(root, fol)
+            img_name = fol.split(".")[0]
+            ma =  img_name + "_MA.tif"
+            he = img_name + "_HE.tif"
+            ex = img_name + "_EX.tif"
+            se = img_name + "_SE.tif"
+            od = img_name + "_OD.tif"
+
+            img = cv2.imread(root+fol, cv2.IMREAD_GRAYSCALE)
+            img = self.transform(img)
+
+            msk = np.zeros((512, 512))
+            h, w = 512, 512
+            
+
+            img_ma = self.return_image_abnormality(dataset_type, "1. Microaneurysms/", ma, h, w)
+            img_he = self.return_image_abnormality(dataset_type, "2. Haemorrhages/", he, h, w)
+            img_ex = self.return_image_abnormality(dataset_type, "3. Hard Exudates/", ex, h, w)
+            img_se = self.return_image_abnormality(dataset_type, "4. Soft Exudates/", se, h, w)
+            img_od = self.return_image_abnormality(dataset_type, "5. Optic Disc/", od, h, w)
+
+            msk[img_od > 0] = self.class_id_abnormality["OD"]
+            msk[img_se > 0] = self.class_id_abnormality["SE"]
+            msk[img_ex > 0] = self.class_id_abnormality["EX"]
+            msk[img_he > 0] = self.class_id_abnormality["HE"]
+            msk[img_ma > 0] = self.class_id_abnormality["MA"]
+
+
+            imgs = f"/Users/akhilgattu/Desktop/VLM_project/{img_root}/{dataset_type}/images/{fol}"
+            masks = f"/Users/akhilgattu/Desktop/VLM_project/{img_root}/{dataset_type}/masks/{img_name}.tif"
+            cv2.imwrite(imgs, img)
+            cv2.imwrite(masks, msk)
+
+'''
+Class that uses torch.utils.data module to generate (img, mask) data 
+batchwise with a batch size.
+'''
+class SegDataSet(Dataset):
+    def __init__(self, image_paths, mask_paths):
+        self.image_paths = image_paths
+        self.mask_paths = mask_paths
+        self.image_paths = [self.image_paths+image_path for image_path in sorted(os.listdir(self.image_paths))]
+        self.mask_paths = [self.mask_paths+mask_path for mask_path in sorted(os.listdir(self.mask_paths))]
+
+
+    def __len__(self):
+        return len(self.image_paths)
+    
+    def __getitem__(self, idx):
+        img_path = self.image_paths[idx]
+        mask_path = self.mask_paths[idx]
+
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = torch.from_numpy(image).permute(2, 0, 1).float() / 255.0
+
+        mask = cv2.imread(mask_path, cv2.IMREAD_UNCHANGED)
+        mask = torch.from_numpy(mask)
+        
+        multilabel_mask = torch.zeros([5, 512, 512], dtype=torch.float32)
+        for cls_id in range(1, 6):
+            multilabel_mask[cls_id - 1] = (mask == cls_id)
+
+        return image, multilabel_mask
---
+++ main.py
@@ -0,0 +1,81 @@
+from generate_load_dataset import SegDataSet, IDRiDDatasetBuilder
+from torch.utils.data import DataLoader, Dataset
+import torch
+import segmentation_models_pytorch as smp
+import torch.nn
+from tqdm import tqdm
+
+
+image_paths = "/Users/akhilgattu/Desktop/VLM_project/Data/train/images/"
+mask_paths = "/Users/akhilgattu/Desktop/VLM_project/Data/train/masks/"
+
+image_test_paths = "/Users/akhilgattu/Desktop/VLM_project/Data/test/images/"
+mask_test_paths = "/Users/akhilgattu/Desktop/VLM_project/Data/test/masks/"
+
+DEVICE = 'mps' if torch.mps.is_available() else 'cpu'
+
+dataset_builder_train = IDRiDDatasetBuilder("train")
+dataset_builder_test = IDRiDDatasetBuilder("test")
+
+class_to_id = dataset_builder_train.class_id_abnormality
+
+dataset_builder_train.create_dataset("train")
+dataset_builder_test.create_dataset("test")
+
+seg_dataset = SegDataSet(image_paths, mask_paths)
+seg_dataloader = DataLoader(
+    dataset=seg_dataset,
+    batch_size=3
+)
+
+seg_test_dataset = SegDataSet(image_paths, mask_paths)
+seg_test_dataloader = DataLoader(
+    dataset=seg_test_dataset,
+    batch_size=3
+)
+
+#Use UNET++ model with timm-efficientnet-b3 encode and imagenet encoder weights
+model = smp.UnetPlusPlus(
+        encoder_name="timm-efficientnet-b3",
+        encoder_weights="imagenet",
+        in_channels=3,
+        classes=5,
+        activation=None,
+)
+
+
+DiceLoss = smp.losses.DiceLoss
+FocalLoss = smp.losses.FocalLoss
+
+dice_loss = DiceLoss('multilabel', from_logits=True)
+focal_loss = FocalLoss('multilabel', gamma=1.5)
+bce = torch.nn.BCEWithLogitsLoss()
+
+optimizer = torch.optim.AdamW(model.parameters())
+
+EPOCHS = 10
+model = model.to(DEVICE)
+for ep in range(EPOCHS):
+    model.train()
+    total_loss = 0
+    for img, mask in tqdm(seg_dataloader, desc=f"Train {ep}/{EPOCHS}"):
+        img = img.to(DEVICE)
+        mask = mask.to(DEVICE)
+        output = model(img)
+        optimizer.zero_grad()
+        loss = 0.5 * dice_loss(output, mask) + 0.5 * bce(output, mask)
+        total_loss += loss.item()
+        loss.backward()
+        optimizer.step()
+    print(total_loss/max(1, len(seg_dataloader)))
+
+total_loss = 0
+model = model.to('mps')
+with torch.no_grad():
+    for img, mask in tqdm(seg_test_dataloader):
+            img = img.to(DEVICE)
+            mask = mask.to(DEVICE)
+            output = model(img)
+            loss = 0.5 * dice_loss(output, mask) + 0.5 * bce(output, mask)
+            total_loss += loss.item()
+    print(total_loss/max(1, len(seg_test_dataloader)))
